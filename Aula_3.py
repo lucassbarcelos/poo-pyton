d95bf9c30380b45ba8e38437f7285c6f95587a4e
Lucas <lucasbarcelos58@gmail.com>
2015-08-18 10:37:20 -0700

    testes ipsum lorum

--- Aula_3.py
+++ Aula_3.py
@@ -1,1 +1,19 @@
-__author__ = 'khcjchj'
+#__author__ = 'Lucas Barcelos'
+print(2**63-1)
+print(3/2)
+
+c=1+1j
+print(type(c))
+print(c**3)
+
+c='c'
+s="Lucas"
+x='''Lucas
+
+Barcelos'''
+
+print(x)
+print(type(x))
+print(type(s))
+print(type(c))
+print("ipsum lorum"*10)
